2905a028a4600398e9467a0c019665bf64bb8f05
lucasnrodriguez <lucasrodriguezlks@gmail.com>
2024-07-11 23:05:06 -0300

    act

---
+++ js/login.js
@@ -0,0 +1,13 @@
+/* $(document).ready(function () {
+    setTimeout(function () {
+        $(".EmpreStock").fadeOut(600);
+    }, 800);
+
+    setTimeout(function () {
+        $(".EmpreStock2").fadeIn(600);
+    }, 1200);
+
+    setTimeout(function () {
+        $(".buttons-container.").fadeIn(600);
+    }, 1200);
+}); */
